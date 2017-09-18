eccfa233c43eb15d4c3018d830db986064a73d13
Clewler <eurobi.123@gmail.com>
2017-09-18 10:46:47 +0200

    main cleanup

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <fstream>
 #include <vector>
-#define OUTPUT 8
 
 enum rlcMode {T, U5, U10, A};
 enum typeSdu {D, C};
@@ -15,63 +14,19 @@ struct RlcSduS
     int sizePdu;
     int seqNum;
     std::vector<int> sizeSduS;
-    std::vector<string> data;
+    std::vector<std::string> data;
 };
 
 struct RlcPduS
 {
     rlcMode mode;
     int sizePdu;
-    std::vector<string> data;
+    std::vector<std::string> data;
 };
 
-void decToBin(int hex){
-    ofstream file;
-    file.open("output.txt", ios::app);
-    string output = "";
-    while(hex!=0){
-        if(hex%2){
-            output+="1";
-            hex--;
-        }
-        else
-            output+="0";
-        hex/=2;
-    }
-    if (output.length()!=OUTPUT)
-        {
-            int temp = OUTPUT - output.length();
-            for(int i=0;i<temp;i++){
-                output+="0";
-            }
-        }
-    for(int i=output.length(); i>=0;i--){
-        
-        file <<output[i];
 
-    }
-    file <<" ";
-    file.close();
-}
 int main()
 {
-int hex = 0;
-string x;
-std::ofstream ofs;
-ofs.open("output.txt", std::ofstream::out | std::ofstream::trunc);
-ofs.close();
-
-ifstream file;
-file.open("input");
-while(!file.eof()){
-    //file >> std::hex >> hex;
-    file >> std::hex >> hex;
-    
-    decToBin(hex);
-
+    return 0;
 }
-file.close();
 
-std::cout << std::endl;
-return 0;
-}
