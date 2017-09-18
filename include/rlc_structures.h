b838eefc047c389105ca6f289b7bc5dee1e0dac9
adamskiu <adam.wroblak@gmail.com>
2017-09-18 11:28:40 +0200

    Moved structs definitions to separate header file.

---
+++ include/rlc_structures.h
@@ -0,0 +1,25 @@
+#ifndef RLC_STRUCTURES_H
+#define RLC_STRUCTURES_H
+
+#include <vector>
+enum rlcMode {T, U5, U10, A};
+enum typeSdu {D, C};
+
+struct RlcSduS
+{
+    rlcMode mode;
+    typeSdu type;
+    int pool;
+    int sizePdu;
+    int seqNum;
+    std::vector<int> sizeSduS;
+    std::vector<std::string> data;
+};
+
+struct RlcPduS
+{
+    rlcMode mode;
+    int sizePdu;
+    std::vector<std::string> data;
+};
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,3 @@
-#include <iostream>
-#include <stdlib.h>
-#include <fstream>
-#include <vector>
-
-enum rlcMode {T, U5, U10, A};
-enum typeSdu {D, C};
-
-struct RlcSduS
-{
-    rlcMode mode;
-    typeSdu type;
-    int pool;
-    int sizePdu;
-    int seqNum;
-    std::vector<int> sizeSduS;
-    std::vector<std::string> data;
-};
-
-struct RlcPduS
-{
-    rlcMode mode;
-    int sizePdu;
-    std::vector<std::string> data;
-};
-
 
 int main()
 {
