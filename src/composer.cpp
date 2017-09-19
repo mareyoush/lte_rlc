8831bdd164eee01cb8b03a9c9f44d8693d313159
nowakjakub <novik21e@gmail.com>
2017-09-19 16:05:43 +0200

    Replaced regex.

--- src/composer.cpp
+++ src/composer.cpp
@@ -1,30 +1,27 @@
 #include "composer.h"
 #include <fstream>
 #include <iostream>
-#include <regex>
 #include <sstream>
 
+
+//Autor: Jakub Nowak
 int loadSduFile(std::string filename, RlcSduS *rlcSdu_p)
 {
     std::string line;
-    std::string delimiter = " ";
+    
     
     std::ifstream file(filename.c_str());
     
     getline(file, line);
     
-    //int i = 0;
 
-    if(std::regex_search(line, std::regex("^T")))
+    std::istringstream var(line);
+    std::string mode = "", type_or_data = "";
+    int pool = 0, sizePdu=0;
+    var >> mode >> type_or_data >> pool >> sizePdu;
+
+    if(mode == "T")
     {
-        std::istringstream var(line);
-        std::string a = "",b = "";
-        int c = 0, d=0;
-
-        var >> a >> b >> c >> d;
-
-        std::cout << a << std::endl << b << std::endl << c << std::endl << d << std::endl; 
-
         rlcSdu_p->mode = T;
         while(getline(file, line))
         {
@@ -33,26 +30,21 @@ int loadSduFile(std::string filename, RlcSduS *rlcSdu_p)
         }
         
     } 
-    else if(std::regex_search(line, std::regex("^A")))
+    else if(mode == "A")
     {
         rlcSdu_p->mode = A;
-        if(std::regex_search(line, std::regex("D")))
+        if(type_or_data == "D")
         {
             rlcSdu_p->type = D;
+            rlcSdu_p->pool = pool;
+            rlcSdu_p->sizePdu = sizePdu;
+            while(getline(file, line))
+            {
+                rlcSdu_p->data.push_back(line);
+            }
             
-            std::istringstream var(line);
-            std::string a,b;
-            int c, d;
-
-            var >> a >> b >> c >> d;
-
-            std::cout << a << std::endl << b << std::endl << c << std::endl << d << std::endl; 
-            
-            /*std::string::iterator end_pos = std::remove(pool.begin(), pool.end(), ' ');
-            pool.erase(end_pos, pool.end());
-            rlcSdu_p->pool = stoi(pool);*/
         }
-        else if(std::regex_search(line, std::regex("C")))
+        else if(type_or_data == "C")
         {
             std::string tmp_SN;
             while(getline(file, line))
@@ -62,30 +54,22 @@ int loadSduFile(std::string filename, RlcSduS *rlcSdu_p)
             rlcSdu_p->data.push_back(tmp_SN);
         }
     }
-    else if(std::regex_search(line, std::regex("^U5")))
+    else if(mode == "U5")
     {
         rlcSdu_p->mode = U5;
 
-        std::string sizePdu = line.substr(line.find(delimiter));
-
-        std::string::iterator end_pos = std::remove(sizePdu.begin(), sizePdu.end(), ' ');
-        sizePdu.erase(end_pos, sizePdu.end());
-        rlcSdu_p->sizePdu = stoi(sizePdu);
+        rlcSdu_p->sizePdu = stoi(type_or_data);
 
         while(getline(file, line))
         {
             rlcSdu_p->data.push_back(line);
         }
     }
-    else if(std::regex_search(line, std::regex("^U10")))
+    else if(mode == "U10")
     {
         rlcSdu_p->mode = U10;
         
-        std::string sizePdu = line.substr(line.find(delimiter));
-        std::string::iterator end_pos = std::remove(sizePdu.begin(), sizePdu.end(), ' ');
-        sizePdu.erase(end_pos, sizePdu.end());
-        rlcSdu_p->sizePdu = stoi(sizePdu);
-        std::cout << sizePdu;
+        rlcSdu_p->sizePdu = stoi(type_or_data);
         while(getline(file, line))
         {
             rlcSdu_p->data.push_back(line);
