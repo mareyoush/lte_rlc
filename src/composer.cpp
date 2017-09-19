895517fb196f95723c1b9a247d55a71b3449bd5e
nowakjakub <novik21e@gmail.com>
2017-09-19 11:03:34 +0200

    Add C++11 in makefile, begining of regex in composer.cpp

--- src/composer.cpp
+++ src/composer.cpp
@@ -1,15 +1,34 @@
 #include "composer.h"
 #include <fstream>
 #include <iostream>
+#include <regex>
 
 int loadSduFile(const std::string filename)//, RlcSduS *rlcSdu_p)
 {
     char character;
     std::ifstream file(filename.c_str());
-    while(file.get(character)){ 
+    /*while(file.get(character)){ 
+        if (character == ' ')
+            continue;
         std::cout << character;
-    }  
+    } */ 
+    std::string line;
+    getline(file, line);
+    //std::cout << line;
+
+    std::regex pattern("^T");
+
+    if(std::regex_search(line, pattern))
+    {
+        file.get(character);
+        std::cout << character << std::endl;
+    } 
+
+    file.get(character);
+    std::cout << character;
+
     file.close();
+
     std::cout << std::endl;
     return 0;
 }
