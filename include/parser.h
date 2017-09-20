ec86212ea872e35525bed03f91e4b30642ee234f
mareyoush <1mariuszkoziel@gmail.com>
2017-09-20 09:54:29 +0200

    Added function to convert hex string to binary string

--- include/parser.h
+++ include/parser.h
@@ -2,12 +2,14 @@
 #define PARSER_H
 #include "rlc_structures.h"
 #include <stdint.h>
+#include <string>
 
 #define EFILE_EMPTY 1
 #define EBAD_FORMAT 2
 #define EBAD_PDU_SIZE 3
 #define EBAD_DATA_LENGTH 4
 
+std::string hexToBin(std::string hex);
 uint16_t check_pdu_file(FILE *file, struct RlcPduS *pdu);
 uint16_t rlcParser(RlcPduS *rlcPdu_p, RlcSduS *rlcSdu_p);
 uint16_t parseU5(RlcPduS *pdu, RlcSduS *sdu);
